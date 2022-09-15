98921081d96a1b1ad935378380e336bd79541fff
“CGKIM” <cgkim@eduwill.net>
2022-09-15 18:08:10 +0900

    Update MainActivity.kt

--- app/src/main/java/com/dynast/replycompose/MainActivity.kt
+++ app/src/main/java/com/dynast/replycompose/MainActivity.kt
@@ -6,30 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.dynast.replycompose.ui.nav.NavGraph
 import com.dynast.replycompose.ui.nav.NavItem
 import com.dynast.replycompose.ui.theme.ReplyComposeTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReplyComposeTheme {
                 val navController = rememberNavController()
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentRoute = navBackStackEntry?.destination?.route
 
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-//                    SearchContent()
-//                    EmailContent()
-//                    ComposeContent()
-//                    HomeContent()
                     NavGraph(navController = navController, startDestination = NavItem.Home.route)
                 }
             }
